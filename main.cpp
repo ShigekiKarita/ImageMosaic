25f4d132ccf1bf30d3465873e770579a0e0c5f49
ShigekiKarita <shigekikarita@gmail.com>
2014-06-17 19:41:54 +0900

    fix tab

--- main.cpp
+++ main.cpp
@@ -32,37 +32,37 @@ int main(int argc, char *argv[])
 			convert_to_filename(level, i, filenames);
 			MyKeyPoints kp(filenames[0], filenames[1]);
 			
-            cv::Mat_< cv::Vec3b > rgb_image1 = cv::imread(filenames[0], 1);
+            		cv::Mat_< cv::Vec3b > rgb_image1 = cv::imread(filenames[0], 1);
 			cv::Mat_< cv::Vec3b > rgb_image2 = cv::imread(filenames[1], 1);
             
 			const auto startTime = std::chrono::system_clock::now();
 	
-            // select feature
-            if (argc > 1 && strstr(argv[1], "adjust"))
-            {
-                std::cout << "adjust\n";
-                kp.adjust_init();
-            }
-            else
-            {
-                kp.init();
-            }
+			// select feature
+			if (argc > 1 && strstr(argv[1], "adjust"))
+			{
+		                std::cout << "adjust\n";
+		                kp.adjust_init();
+			}
+			else
+			{
+				kp.init();
+			}
             
             
-            // select using RGBs or not
-            if (argc > 1 && std::strstr(argv[1], "threshold") != NULL)
-            {
-                mosaic_with_helmat(rgb_image1, rgb_image2, kp.points1, kp.points2, Helmat::threshold);
-            }
-            else if (argc > 1 && std::strstr(argv[1], "weight") != NULL)
-            {
-                mosaic_with_helmat(rgb_image1, rgb_image2, kp.points1, kp.points2, Helmat::weight);
-            }
-            else
-            {
-                mosaic_with_helmat(rgb_image1, rgb_image2, kp.points1, kp.points2);
-            }
-            
+			// select using RGBs or not
+			if (argc > 1 && std::strstr(argv[1], "threshold") != NULL)
+			{
+				mosaic_with_helmat(rgb_image1, rgb_image2, kp.points1, kp.points2, Helmat::threshold);
+			}
+			else if (argc > 1 && std::strstr(argv[1], "weight") != NULL)
+			{
+				mosaic_with_helmat(rgb_image1, rgb_image2, kp.points1, kp.points2, Helmat::weight);
+			}
+			else
+			{
+				mosaic_with_helmat(rgb_image1, rgb_image2, kp.points1, kp.points2);
+			}
+
 			const auto endTime = std::chrono::system_clock::now();
 			const auto timeSpan = endTime - startTime;
 
