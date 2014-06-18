97b4c63c8f882bcc08df2dda93a870b42e3deef5
Shigeki Karita <shigekikarita@gmail.com>
2014-06-18 17:03:42 +0900

    prepare result images and delete unused things

--- mytransforms.hpp
+++ mytransforms.hpp
@@ -15,16 +15,8 @@ T max(T const & a, T const & b)
 	return a < b ? b : a;
 }
 
-
-void show(std::string image)
-{
-	const cv::Mat input = cv::imread(image, 1);	
-	cv::imshow(image, input);
-}
-
-
 template <class T = cv::Mat_< cv::Vec3b >>
-void display_mosaiced_image(T& image_to_write, const T& image_to_read, const std::string& name)
+void display_mosaiced_image(T& image_to_write, const T& image_to_read, const std::string& name, const bool is_write = false)
 {
 	for ( int y = 0; y < image_to_read.rows; ++y )
 	{
@@ -36,41 +28,16 @@ void display_mosaiced_image(T& image_to_write, const T& image_to_read, const std
 
 	cv::namedWindow(name, cv::WINDOW_AUTOSIZE);
 	cv::imshow(name, image_to_write);
+
+    if (is_write)
+    {
+        const auto out_name = "result_img/" + name + ".png";
+        cv::imwrite(out_name, image_to_write);
+    }
 }
 
-
-template <class T = cv::Mat_< cv::Vec3b >, class U = cv::Point>
-void mosaic_with_homography(const T& rgb_image1, const T& rgb_image2, const U& points1, const U& points2)
-{
-	// 画像1を画像2の空間に投影
-	auto homography = cv::findHomography(points1, points2, CV_RANSAC);
-	cv::Mat_< cv::Vec3b > result;
-	cv::warpPerspective(rgb_image1, result, homography, cv::Size(( ( int ) rgb_image1.cols * 2 ), rgb_image1.rows * 2));
-	display_mosaiced_image(result, rgb_image2, "Homography");
-}
-
-
-template <class T = cv::Mat_< cv::Vec3b >, class U = cv::Point>
-void mosaic_with_affine(const T& rgb_image1, const T& rgb_image2, const U& points1, const U& points2)
-{
-	auto warp_dst = cv::Mat::zeros(rgb_image1.rows, rgb_image1.cols, rgb_image1.type());
-	cv::Point2f srcTri[3];
-	cv::Point2f dstTri[3];
-
-	for ( size_t i = 0; i < 3; ++i )
-	{
-		srcTri[i] = cv::Point2f(points1[i][0], points1[i][1]);
-		dstTri[i] = cv::Point2f(points2[i][0], points2[i][1]);
-	}
-
-	cv::Mat warp_mat = getAffineTransform(srcTri, dstTri);
-	cv::Mat_< cv::Vec3b > result;
-	warpAffine(rgb_image1, result, warp_mat, cv::Size(( ( int ) rgb_image1.cols * 2 ), rgb_image1.rows * 2));
-	display_mosaiced_image(result, rgb_image2, "Affine");
-}
-
-
-cv::Mat_<double> least_mean_square_helmat(std::vector< cv::Vec2f > source, std::vector< cv::Vec2f > distination, size_t new_size = 5)
+cv::Mat_<double>
+least_mean_square_helmat(std::vector< cv::Vec2f > source, std::vector< cv::Vec2f > distination, size_t new_size = 5)
 {
 	source.resize(new_size);
 	distination.resize(new_size);
@@ -141,6 +108,7 @@ void sieve_with_RGB_distance(const T& rgb_source, const T& rgb_distination, U& s
 	{
 		const auto rhs = rgb_source.template at<cv::Vec3b>(check_height(source_keypoints[i][1]), check_width(source_keypoints[i][0]));
 		const auto lhs = rgb_distination.template at<cv::Vec3b>(check_height(distination_keypoints[i][1]), check_width(distination_keypoints[i][0]));
+        
 		auto d = rgb_euclid_distance(rhs, lhs);
 
 		if ( threshold > d )
@@ -221,7 +189,7 @@ cv::Mat_<double> RGB_weighted_helmat(const T& rgb_source, const T& rgb_distinati
 		A(i, 2) = 1;
 		A(i, 3) = 0;
 
-		auto j = i + size;
+		const auto j = i + size;
 		A(j, 0) = source_keypoints[i][1];
 		A(j, 1) = -source_keypoints[i][0];
 		A(j, 2) = 0;
@@ -230,8 +198,10 @@ cv::Mat_<double> RGB_weighted_helmat(const T& rgb_source, const T& rgb_distinati
 		L(0, i) = distination_keypoints[i][0];
 		L(0, j) = distination_keypoints[i][1];
 
-		const auto rhs = rgb_source.template at<cv::Vec3b>(check_height(source_keypoints[i][1]), check_width(source_keypoints[i][0]));
-		const auto lhs = rgb_distination.template at<cv::Vec3b>(check_height(distination_keypoints[i][1]), check_width(distination_keypoints[i][0]));
+		const auto rhs = rgb_source.template at<cv::Vec3b>(check_height(source_keypoints[i][1]),
+                                                           check_width(source_keypoints[i][0]));
+		const auto lhs = rgb_distination.template at<cv::Vec3b>(check_height(distination_keypoints[i][1]),
+                                                                check_width(distination_keypoints[i][0]));
 
 		R(i, i) = rgb_euclid_distance(rhs, lhs);
 		R(j, j) = R(i, i);
@@ -239,13 +209,13 @@ cv::Mat_<double> RGB_weighted_helmat(const T& rgb_source, const T& rgb_distinati
 
 	A = R * A;
 	L = R * L;
-
 	cv::Mat_<double> K = ( A.t() * A ).inv() * A.t() * L;
 
 	return  cv::Mat_<double>(2, 3) << K(0, 0), K(0, 1), K(0, 2), -K(0, 1), K(0, 0), K(0, 3);
 }
 
-namespace Helmat {
+namespace Helmat
+{
     enum operation
     {
         none = 0,
@@ -254,27 +224,31 @@ namespace Helmat {
     };
 }
 
-template <class T = cv::Mat_< cv::Vec3b >, class U = std::vector< cv::Vec2f >>
-void mosaic_with_helmat(const T& rgb_source, const T& rgb_distination, U& source_keypoints, U& distination_keypoints, Helmat::operation op = Helmat::none,  bool is_display = true)
+template <class T = std::string*, class U = std::vector< cv::Vec2f >>
+void mosaic_with_helmat(const T filenames, U& source_keypoints, U& distination_keypoints, Helmat::operation op = Helmat::none,  bool is_display = true)
 {
     cv::Mat_<double> helmat;
+    cv::Mat_< cv::Vec3b > rgb_source = cv::imread(filenames[0], 1);
+    cv::Mat_< cv::Vec3b > rgb_distination = cv::imread(filenames[1], 1);
     
-    switch (op) {
+    switch (op)
+    {
         case Helmat::threshold:
             helmat = RGB_sieve_LMS_helmat(rgb_source, rgb_distination, source_keypoints, distination_keypoints);
             break;
         case Helmat::weight:
             helmat = RGB_weighted_helmat(rgb_source, rgb_distination, source_keypoints, distination_keypoints);
+            break;
         default:
             helmat = least_mean_square_helmat(source_keypoints, distination_keypoints);
             break;
     }
     
 	cv::Mat_< cv::Vec3b > result;
-	warpAffine(rgb_source, result, helmat, cv::Size(( ( int ) rgb_source.cols * 2 ), rgb_source.rows * 2));
+	warpAffine(rgb_source, result, helmat, cv::Size(rgb_source.cols * 2, rgb_source.rows * 2));
 
 	if ( is_display )
 	{
-		display_mosaiced_image(result, rgb_distination, "Helmert");
+		display_mosaiced_image(result, rgb_distination, filenames[3]);
 	}
 }
